6c71c1abb9d8aac1bd215ba7fe4ef8b90d39b393
m1kael1 <mikaelagungdjawa@gmail.com>
2023-10-07 20:17:10 +0700

    init landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalSotrage";
 import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import News from "./pages/News";
 // import NewsHistory from "./pages/NewsHistory";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
+	const { status } = useLocalStorage();
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				{/* <Route path="/news-history" element={<NewsHistory />} /> */}
-				<Route path="/:portal/:category" element={<News />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			{status ? (
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route path="/news-history" element={<NewsHistory />} /> */}
+					<Route path="/:portal/:category" element={<News />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			) : (
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			)}
 		</BrowserRouter>
 	);
 }
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import useLocalStorage from "../hooks/useLocalSotrage";
+
+export default function Landing() {
+	const { portalsData, setPortalsData } = useLocalStorage();
+	const navigate = useNavigate();
+
+	function handleGetStarted() {
+		setPortalsData({ ...portalsData, status: true });
+		navigate(0);
+	}
+
+	return (
+		<div className="h-screen flex flex-col items-center justify-center">
+			<h1 className="text-4xl font-light font-poppins">Selamat Datang </h1>
+			<button
+				onClick={handleGetStarted}
+				className=" inline-flex mt-3 items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+			>
+				Mulai Baca Berita
+				<svg
+					className="w-3.5 h-3.5 ml-2 "
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 14 10"
+				>
+					<path
+						stroke="currentColor"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth={2}
+						d="M1 5h12m0 0L9 1m4 4L9 9"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
+}
